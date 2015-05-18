9218f6ebcb0647cbd42153bbfa3f3c378d11119f
Raindeezy <roviir@gmail.com>
2015-05-18 15:32:21 -0700

    user model

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,1 +1,52 @@
 'use strict';
+
+var User = require('../models/User');
+var bodyparser = require('body-parser');
+
+module.exports = function(router, passport) {
+		router.use(bodyparser.json());
+
+		router.post('/create_user', function(req, res) {
+			var newUserData = JSON.parse(JSON.stringify(req.body));
+			delete newUserData.email;
+			delete newUserData.password;
+			var newUser = new User(newUserData);
+			newUser.email = req.body.email;
+			newUser.generateHash(req.body.password, 8, function (err, hash) {
+				if (err) {
+					console.log(err)
+				}
+
+				newUser.password = hash;
+				saveFunc();
+			});
+			function saveFunc() {
+			newUser.save(function(err, user) {
+				if(err) {
+					console.log(err);
+					return res.status(500).json({msg: 'could not create user'});
+				}
+
+				user.generateToken(process.env.APP_SECRET, function(err, token) {
+					if(err) {
+						console.log(err);
+						return res.status(500).json({msg: 'error generating token'});
+					}
+
+					res.json({token: token});	
+				});
+			});
+			};
+		});
+
+		router.get('/sign_in', passport.authenticate('basic', {session: false}), function(req, res) {
+			req.user.generateToken(process.env.APP_SECRET, function (err, token) {
+				if (err) {
+					console.log(err);
+					return res.status(500).json({msg: 'error generating token'});
+				}
+
+				res.json({token: token});
+			});
+		});
+};
--- server.js
+++ server.js
@@ -4,10 +4,18 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var EventEmitter = require('events').EventEmitter;
+var passport = require('passport');
+var usersRoutes = express.Router();
+
+process.env.APP_SECRET = process.env.APP_SECRET || 'changethis!';
 
 //Routes (and app.use calls) go here
+app.use(passport.initialize());
+require('./lib/passport_strategy')(passport);
+require('./routes/user_routes')(usersRoutes, passport);
 
 
+app.use('/api', usersRoutes);
 
 //Prepare our emitter (to notify Mocha that we're ready to accept connections)
 var serverEmitter = module.exports = exports = new EventEmitter();
