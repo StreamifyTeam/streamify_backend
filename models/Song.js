9106fe64ef879baa868ec7655dd160437c50b81e
Truyen Nguyen <truyen.m.nguyen@gmail.com>
2015-05-21 11:48:22 -0700

    Add album artwork url to Song Schema

--- models/Song.js
+++ models/Song.js
@@ -7,6 +7,7 @@ var songSchema = mongoose.Schema({
 	duration: String,
 	album: String,
 	spotifyID: {type: String, unique: true},
+	album_artwork_url: {type: String, unique: true}
 });
 
 module.exports = mongoose.model('Song', songSchema);
